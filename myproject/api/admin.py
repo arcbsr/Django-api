910017c9a48de5ab10ba669fbe78db16ed202010
Md Rafiur Rahman <rafiur.r@frog.social>
2024-09-10 14:44:24 +0100

    - delete previous migrations

--- myproject/api/admin.py
+++ myproject/api/admin.py
@@ -6,6 +6,14 @@ from .apimodules.Room import *
 
 # Register your models here.
 admin.site.register(UserProfile)
-admin.site.register(CustomUser)
-admin.site.register(Room)
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in CustomUser._meta.fields]
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Room._meta.fields]
+    search_fields = ['room_name']
+
 admin.site.register(Message)
--- myproject/api/apimodules/Room.py
+++ myproject/api/apimodules/Room.py
@@ -6,11 +6,18 @@ class Room(models.Model):
 
     def __str(self):
         return self.room_name
-    
+
+
+class MoreRooms(models.Model):
+    room_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.room_name
 
 class Message(models.Model):
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
     sender = models.CharField(max_length=255)
+    details = models.TextField()
     message = models.TextField()
 
     def __str(self):
